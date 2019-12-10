86ca50e14ed980cbf43f17eeb77607c8d4110ac4
Arun Subramaniam <arunsub27@gmail.com>
2019-12-10 19:15:25 +0000

    first commit

---
+++ PersonalProjectTool/src/main/java/com/arxvn/PersonalProjectTool/PersonalProjectToolApplication.java
@@ -0,0 +1,13 @@
+package com.arxvn.PersonalProjectTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectToolApplication.class, args);
+	}
+
+}
